960bf9a8cbe3f5ccc6006df3b460fcb39192e698
aidabu <ai.ad990@gmail.com>
2020-01-26 18:36:22 +0100

    initial commit

---
+++ src/main/java/es/aida/gamerol/GamerolApplication.java
@@ -0,0 +1,13 @@
+package es.aida.gamerol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamerolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamerolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/aida/gamerol/GamerolApplicationTests.java
@@ -0,0 +1,13 @@
+package es.aida.gamerol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamerolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
